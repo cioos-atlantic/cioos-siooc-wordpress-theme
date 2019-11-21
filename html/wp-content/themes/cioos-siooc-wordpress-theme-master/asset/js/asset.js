0f5752a07c4a9546e4044b41114bc9053d1b9d86
Joel <joel.friesen@hakaimagazine.com>
2019-11-21 14:32:14 -0800

    Language update

--- html/wp-content/themes/cioos-siooc-wordpress-theme-master/asset/js/asset.js
+++ html/wp-content/themes/cioos-siooc-wordpress-theme-master/asset/js/asset.js
@@ -235,7 +235,7 @@ function setVerticalFilters( minVertical, maxVertical )
         });
 
         $.ajax({
-            url: "/wp-content/themes/cioos-siooc-wordpress-theme-master/asset/resources/ckan.json",
+            url: "/wp-content/themes/cioos-siooc-wordpress-theme-master/asset/resources/pacific.json",
             dataType: 'json',
             async: false,
             success: function (data) {
