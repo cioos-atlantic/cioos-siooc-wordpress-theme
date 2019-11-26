54eef32cefa2c31b2baa1a6853133e90d21e8432
Joel <joel.friesen@hakaimagazine.com>
2019-11-26 10:48:39 -0800

    footer css

--- html/wp-content/themes/cioos-siooc-wordpress-theme-master/functions.php
+++ html/wp-content/themes/cioos-siooc-wordpress-theme-master/functions.php
@@ -22,54 +22,54 @@
 
 
 
-//Dequeue JavaScripts
-function dequeue_unnecessary_scripts() {
-    wp_dequeue_script( 'backbone');
-    wp_dequeue_script( 'backbone-marionette');
-    wp_dequeue_script( 'backbone-radio');
-    wp_dequeue_script( 'bellows');
-    wp_dequeue_script( 'bootstrap' ); 
-    wp_dequeue_script( 'carouFredSel' ); 
-    wp_dequeue_script( 'comment-reply' );
-    wp_dequeue_script( 'elementor-common');
-    wp_dequeue_script( 'elementor-common-modules');
-    wp_dequeue_script( 'elementor-dialog');
-    wp_dequeue_script( 'elementor-frontend');
-    wp_dequeue_script( 'elementor-frontend-modules');
-    wp_dequeue_script( 'elementor-waypoints');
-    wp_dequeue_script( 'hoverIntent');
-    wp_dequeue_script( 'jquery-masonry' );
-    wp_dequeue_script( 'jquery-scrollup' ); 
-    wp_dequeue_script( 'jquery-ui-core' ); 
-    wp_dequeue_script( 'jquery-ui-draggable');
-    wp_dequeue_script( 'jquery-ui-mouse' );
-    wp_dequeue_script( 'jquery-ui-position');
-    wp_dequeue_script( 'jquery-ui-widget' );
-    wp_dequeue_script( 'knob' );
-    wp_dequeue_script( 'megamenu');
-    wp_dequeue_script( 'megamenu-pro');
-    wp_dequeue_script( 'modernizr' );
-    wp_dequeue_script( 'modernizr-js' );
-    wp_dequeue_script( 'prettyPhoto' ); 
-    wp_dequeue_script( 'responsiveslides' );
-    wp_dequeue_script( 'sticky' );
-    wp_dequeue_script( 'swiper');
-    wp_dequeue_script( 'thinkup-backend' ); 
-    wp_dequeue_script( 'thinkup-bootstrap' ); 
-    wp_dequeue_script( 'thinkup-confirm' ); 
-    wp_dequeue_script( 'thinkup-frontend' ); 
-    wp_dequeue_script( 'thinkup-portfolio' ); 
-    wp_dequeue_script( 'thinkup-quicksand' ); 
-    wp_dequeue_script( 'thinkup-quicksand-scale');
-    wp_dequeue_script( 'thinkup-responsiveslides' ); 
-    wp_dequeue_script( 'thinkupverification');
-    wp_dequeue_script( 'tweetscroll');
-    wp_dequeue_script( 'underscore');
-    wp_dequeue_script( 'videobg' );
-    wp_dequeue_script( 'waypoints' );
-    wp_dequeue_script( 'waypoints-sticky' ); 
-}
-add_action( 'wp_print_scripts', 'dequeue_unnecessary_scripts', 1);
+// //Dequeue JavaScripts
+// function dequeue_unnecessary_scripts() {
+//     wp_dequeue_script( 'backbone');
+//     wp_dequeue_script( 'backbone-marionette');
+//     wp_dequeue_script( 'backbone-radio');
+//     wp_dequeue_script( 'bellows');
+//     wp_dequeue_script( 'bootstrap' ); 
+//     wp_dequeue_script( 'carouFredSel' ); 
+//     wp_dequeue_script( 'comment-reply' );
+//     wp_dequeue_script( 'elementor-common');
+//     wp_dequeue_script( 'elementor-common-modules');
+//     wp_dequeue_script( 'elementor-dialog');
+//     wp_dequeue_script( 'elementor-frontend');
+//     wp_dequeue_script( 'elementor-frontend-modules');
+//     wp_dequeue_script( 'elementor-waypoints');
+//     wp_dequeue_script( 'hoverIntent');
+//     wp_dequeue_script( 'jquery-masonry' );
+//     wp_dequeue_script( 'jquery-scrollup' ); 
+//     wp_dequeue_script( 'jquery-ui-core' ); 
+//     wp_dequeue_script( 'jquery-ui-draggable');
+//     wp_dequeue_script( 'jquery-ui-mouse' );
+//     wp_dequeue_script( 'jquery-ui-position');
+//     wp_dequeue_script( 'jquery-ui-widget' );
+//     wp_dequeue_script( 'knob' );
+//     wp_dequeue_script( 'megamenu');
+//     wp_dequeue_script( 'megamenu-pro');
+//     wp_dequeue_script( 'modernizr' );
+//     wp_dequeue_script( 'modernizr-js' );
+//     wp_dequeue_script( 'prettyPhoto' ); 
+//     wp_dequeue_script( 'responsiveslides' );
+//     wp_dequeue_script( 'sticky' );
+//     wp_dequeue_script( 'swiper');
+//     wp_dequeue_script( 'thinkup-backend' ); 
+//     wp_dequeue_script( 'thinkup-bootstrap' ); 
+//     wp_dequeue_script( 'thinkup-confirm' ); 
+//     wp_dequeue_script( 'thinkup-frontend' ); 
+//     wp_dequeue_script( 'thinkup-portfolio' ); 
+//     wp_dequeue_script( 'thinkup-quicksand' ); 
+//     wp_dequeue_script( 'thinkup-quicksand-scale');
+//     wp_dequeue_script( 'thinkup-responsiveslides' ); 
+//     wp_dequeue_script( 'thinkupverification');
+//     wp_dequeue_script( 'tweetscroll');
+//     wp_dequeue_script( 'underscore');
+//     wp_dequeue_script( 'videobg' );
+//     wp_dequeue_script( 'waypoints' );
+//     wp_dequeue_script( 'waypoints-sticky' ); 
+// }
+// add_action( 'wp_print_scripts', 'dequeue_unnecessary_scripts', 1);
 // =======================================================
 // Enqueue scripts and styles.
 // ======================================================= 
