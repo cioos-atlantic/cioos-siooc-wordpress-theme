2613e8f6f350fae3c8c5afdb160e8ca681e83440
Joel <joel.friesen@hakaimagazine.com>
2020-02-10 09:41:49 -0800

    |

--- html/wp-content/themes/cioos-siooc-wordpress-theme-master/front-page.php
+++ html/wp-content/themes/cioos-siooc-wordpress-theme-master/front-page.php
@@ -12,14 +12,15 @@ get_header(); ?>
 
   <div class="outer-map-container">
     <div class="overlay"></div>
-    <div id="asset_map_container" class="map-container">
-      <div id="map" class="map" style=""></div>
+    <div class="map-container" id="asset_map_container">
+      <div id="map" class="map" ></div>
       <div id="category_panel" class="category-selection"></div>
       <div id="variable_panel" class="variable-selection tab-content"></div>
       <div id="dataset_desc" class="panel_details"></div>
       <div class="panel_search_info" style="text-align:center" id="dataset_search_stats"></div>
     </div>
   </div>
+  
 
 
 <script></script>
